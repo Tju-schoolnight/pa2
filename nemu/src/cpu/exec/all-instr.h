48f3b69d975ab116075cdb425b681bb86210b278
3019244383-Wei Yuhan <615308709@qq.com>
2020-11-03 16:20:25 +0000

    new add 1

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -23,6 +23,34 @@
 #include "logic/shr.h"
 #include "logic/shrd.h"
 
+
+#include "new-add/adc.h"
+#include "new-add/add.h"
+#include "new-add/call.h"
+#include "new-add/cmp.h"
+#include "new-add/ja.h"
+#include "new-add/jae.h"
+#include "new-add/jb.h"
+#include "new-add/jbe.h"
+#include "new-add/je.h"
+#include "new-add/jmp.h"
+#include "new-add/jne.h"
+#include "new-add/jno.h"
+#include "new-add/jnp.h"
+#include "new-add/jns.h"
+#include "new-add/jo.h"
+#include "new-add/jp.h"
+#include "new-add/js.h"
+
+#include "new-add/pop.h"
+#include "new-add/push.h"
+
+#include "new-add/ret.h"
+#include "new-add/sbb.h"
+#include "new-add/sub.h"
+
+#include "new-add/test.h"
+
 #include "string/rep.h"
 
 #include "misc/misc.h"
